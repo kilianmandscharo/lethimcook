1a042fddd0437be162232f83480cdd3242a24f8e
Dominik Heller <kilianmandscharo@gmail.com>
2024-03-16 15:37:35 +0100

    Translate errors the user will see

--- app/errutil/errutil.go
+++ app/errutil/errutil.go
@@ -8,28 +8,28 @@ import (
 type AuthError = error
 
 var (
-	AuthErrorNoAdminFound         AuthError = errors.New("no admin found")
-	AuthErrorDatabaseFailure      AuthError = errors.New("database error")
-	AuthErrorInvalidBody          AuthError = errors.New("invalid body")
-	AuthErrorInvalidPassword      AuthError = errors.New("wrong password provided")
-	AuthErrorPasswordTooLong      AuthError = errors.New("the maximum password length is 72 bytes")
-	AuthErrorPasswordTooShort     AuthError = errors.New("the minimum password length is 5 bytes")
-	AuthErrorAdminAlreadyExists   AuthError = errors.New("there can only be one admin")
-	AuthErrorTokenCreationFailure AuthError = errors.New("failed to create token")
-	AuthErrorInvalidAuthHeader    AuthError = errors.New("invalid auth header provided")
-	AuthErrorInvalidToken         AuthError = errors.New("invalid token provided")
-	AuthErrorInvalidForm          AuthError = errors.New("invalid form")
-	AuthErrorNotAuthorized        AuthError = errors.New("not authorized")
+	AuthErrorNoAdminFound         AuthError = errors.New("Kein Admin gefunden")
+	AuthErrorDatabaseFailure      AuthError = errors.New("Datenbankfehler")
+	AuthErrorInvalidBody          AuthError = errors.New("Ungültiger Body")
+	AuthErrorInvalidPassword      AuthError = errors.New("Falsche Passwort")
+	AuthErrorPasswordTooLong      AuthError = errors.New("Maximale Passwortlänge: 72")
+	AuthErrorPasswordTooShort     AuthError = errors.New("Minimale Passwortlänge: 5")
+	AuthErrorAdminAlreadyExists   AuthError = errors.New("Es kann nur einen Admin geben")
+	AuthErrorTokenCreationFailure AuthError = errors.New("Fehler bei der Tokengenerierung")
+	AuthErrorInvalidAuthHeader    AuthError = errors.New("Ungültiger Auth Header")
+	AuthErrorInvalidToken         AuthError = errors.New("Ungültiger Token")
+	AuthErrorInvalidForm          AuthError = errors.New("Ungültiges Formular")
+	AuthErrorNotAuthorized        AuthError = errors.New("Nicht authorisiert")
 )
 
 type RecipeError = error
 
 var (
-	RecipeErrorInvalidParam    RecipeError = errors.New("invalid path parameter")
-	RecipeErrorInvalidFormData RecipeError = errors.New("invalid form data")
-	RecipeErrorNotFound        RecipeError = errors.New("no recipe found")
-	RecipeErrorDatabaseFailure RecipeError = errors.New("database error")
-	RecipeErrorMarkdownFailure RecipeError = errors.New("error parsing markdown")
+	RecipeErrorInvalidParam    RecipeError = errors.New("Ungültiges Pfadparameter")
+	RecipeErrorInvalidFormData RecipeError = errors.New("Ungültiges Formular")
+	RecipeErrorNotFound        RecipeError = errors.New("Kein Rezept gefunden")
+	RecipeErrorDatabaseFailure RecipeError = errors.New("Datenbankfehler")
+	RecipeErrorMarkdownFailure RecipeError = errors.New("Fehler beim Markdownparsing")
 )
 
 var ErrorHttpCodes = map[error]int{
