01fc79a412f24e8d09ee3da7af2e577d833ed826
Dominik Heller <kilianmandscharo@gmail.com>
2024-09-20 21:30:19 +0200

    fix: readd notification function

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -90,4 +90,38 @@ function attachEventListenerToClipboardButton() {
     });
 }
 
+function notify(message, type) {
+    const notificationContainer = document.getElementById(
+        "notification-container",
+    );
+    if (!notificationContainer) {
+        return;
+    }
+
+    const notifications = document.querySelectorAll(".notification");
+    const index = notifications.length;
+
+    const newNotification = document.createElement("div");
+    newNotification.id = `notification-${index}`;
+    newNotification.className =
+        type === "success"
+            ? "notification notification-success"
+            : "notification notification-danger";
+
+    const newNotificationIcon = document.createElement("div");
+    newNotificationIcon.className =
+        type === "success"
+            ? "fa-solid fa-circle-info"
+            : "fa-regular fa-circle-exclamation";
+
+    const newNotificationText = document.createElement("p");
+    newNotificationText.className = "notification-text";
+    newNotificationText.innerText = message;
+
+    newNotification.appendChild(newNotificationIcon);
+    newNotification.appendChild(newNotificationText);
+
+    notificationContainer.appendChild(newNotification);
+}
+
 main();
